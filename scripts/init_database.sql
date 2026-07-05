b3f2878694257dfc09df58bc7768d1e353bc06c9
Rucha <157823729+trinityr18@users.noreply.github.com>
2026-07-05 14:05:08 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,19 @@
+USE master;
+GO
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
+
 
