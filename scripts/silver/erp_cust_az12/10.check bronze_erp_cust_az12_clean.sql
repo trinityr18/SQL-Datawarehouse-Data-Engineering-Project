6b525055c18a14f2cab28def79e26f6d3240e532
Rucha <157823729+trinityr18@users.noreply.github.com>
2026-07-06 17:22:13 +0530

    Update 10.check bronze_erp_cust_az12_clean.sql

--- scripts/silver/erp_cust_az12/10.check bronze_erp_cust_az12_clean.sql
+++ scripts/silver/erp_cust_az12/10.check bronze_erp_cust_az12_clean.sql
@@ -1,1 +1,15 @@
 
+-- check foreign key 
+select * from [bronze].[erp_cust_az12] where cid like'%AW00011000%'
+
+
+
+--identify out of range dates
+select distinct bdate
+from [bronze].[erp_cust_az12]
+where bdate<'1924-01-01' or bdate>GETDATE()
+
+--data standardization & consistency
+select distinct gen 
+from [bronze].[erp_cust_az12]
+
