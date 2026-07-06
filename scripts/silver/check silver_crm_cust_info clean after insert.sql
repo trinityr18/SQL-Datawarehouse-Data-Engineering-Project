ccb5bb907c366fc2dd60877229c8ab7cc2af83df
Rucha <157823729+trinityr18@users.noreply.github.com>
2026-07-06 14:01:15 +0530

    Update check silver_crm_cust_info clean after insert.sql

--- scripts/silver/check silver_crm_cust_info clean after insert.sql
+++ scripts/silver/check silver_crm_cust_info clean after insert.sql
@@ -1,1 +1,14 @@
+--check for nulls or duplicates in primary key
+select cst_id, count(*)
+from silver.crm_cust_info
+group by cst_id
+having count(*)>1 or cst_id is null
 
+--checck for unwanted spaces
+select cst_firstname from
+silver.crm_cust_info
+where cst_firstname !=TRIM(cst_firstname)
+
+--data standardization & consistency
+select distinct cst_gndr
+from silver.crm_cust_info
