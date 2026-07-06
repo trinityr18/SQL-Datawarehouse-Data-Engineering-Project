4bc53ff9e08be679193cd2f4f388a6aa9707f433
Rucha <157823729+trinityr18@users.noreply.github.com>
2026-07-06 14:20:46 +0530

    Update 6.check silver_crm_prd_info clean after insert.sql

--- scripts/silver/crm.prd_info/6.check silver_crm_prd_info clean after insert.sql
+++ scripts/silver/crm.prd_info/6.check silver_crm_prd_info clean after insert.sql
@@ -1,1 +1,27 @@
 
+--check for nulls or duplicates in primary key
+select prd_id, count(*)
+from silver.crm_prd_info
+group by prd_id
+having count(*)>1 or prd_id is null
+
+--checck for unwanted spaces
+select prd_nm from
+silver.crm_prd_info
+where prd_nm !=TRIM(prd_nm)
+
+--check nulls or negative no.
+select prd_cost
+from silver.crm_prd_info
+where prd_cost<0 or prd_cost is null
+
+
+--data standardization & consistency
+select distinct prd_line
+from silver.crm_prd_info
+
+--check for invalid date orders
+select * from silver.crm_prd_info
+where prd_end_dt<prd_start_dt
+
+select * from silver.crm_prd_info
